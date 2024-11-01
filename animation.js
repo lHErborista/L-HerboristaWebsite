8dd6063aeae3f51fc60426032131f1afef411238
vito di venosa <110898637+Vito290500@users.noreply.github.com>
2024-11-01 16:19:04 +0100

    add new design to chi siamo section

--- animation.js
+++ animation.js
@@ -45,3 +45,50 @@ allh2.forEach(h2 => {
 allh3.forEach(h3 => {
     observer.observe(h3);
 });
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const paragrafiRecensioni = document.querySelectorAll(".recensione-testo");
+
+    function limitaCaratteri() {
+        paragrafiRecensioni.forEach((testoRecensione) => {
+            const testoCompleto = testoRecensione.textContent;
+            let maxCaratteri;
+
+            if (window.innerWidth <= 576) {
+                const primoTitoloRecensione = document.getElementById('prima-recensione')
+                const secondoTitoloRecensione = document.getElementById('seconda-recensione')
+                const terzoTitoloRecensione = document.getElementById('terza-recensione')
+
+                primoTitoloRecensione.textContent = 'Desiree G.';
+                secondoTitoloRecensione.textContent = 'Emanuela Di T.';
+                terzoTitoloRecensione.textContent = 'Michele M.';
+
+                maxCaratteri = 100;  
+            } else if (window.innerWidth <= 768) {
+                maxCaratteri = 120; 
+            } else {
+                maxCaratteri = testoCompleto.length; 
+            }
+
+            if (testoCompleto.length > maxCaratteri) {
+                testoRecensione.textContent = testoCompleto.slice(0, maxCaratteri) + "...";
+            } else {
+                testoRecensione.textContent = testoCompleto;
+            }
+        });
+    }
+
+    limitaCaratteri();
+    window.addEventListener("resize", limitaCaratteri);
+});
+
+
+
+
+
+
+
